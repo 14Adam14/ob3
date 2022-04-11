ef9b0b6bdef9eb7bec70d8f2a25ce5a84923c347
Adam Bolotov <adam.bolotov@gmail.com>
2022-04-11 06:34:23 +0200

    Initial Commit

---
+++ ob3/ob3App.swift
@@ -0,0 +1,17 @@
+//
+//  ob3App.swift
+//  ob3
+//
+//  Created by user219374 on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ob3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
